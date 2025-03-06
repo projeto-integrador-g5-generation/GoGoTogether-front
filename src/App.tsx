0d71ae43646d9ca969f254a9ebc549f30e3219fa
alanaoliv <alanasanches97@gmail.com>
2025-03-06 10:30:02 -0300

    :sparkles: Criação das pages Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Cadastro from "./pages/cadastro/Cadastro";
+import "./index.css";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./context/AuthContext";
+
 function App() {
   return (
     <>
-      <h1>GoGoTogether</h1>
     </>
   );
 }
