c1b238ab1f370ea1de060944f4d6a285443f1dca
alanaoliv <alanasanches97@gmail.com>
2025-03-06 10:31:45 -0300

    :sparkles: Ajuste em App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import Cadastro from "./pages/cadastro/Cadastro";
 import "./index.css";
-import Login from "./pages/login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import { AuthProvider } from "./context/AuthContext";
+
 
 function App() {
   return (
